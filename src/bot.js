e900c57b05c641a5881dc0ec3cf6586d048e1255
clamc068 <laetitia.caffarelli@gmail.com>
2023-05-12 16:56:53 +0200

    Birthday greetings!!!

--- src/bot.js
+++ src/bot.js
@@ -9,4 +9,31 @@ function botResponse(message) {
     return { text: message };
 }
 
-module.exports = bot;
+const users = [
+    {
+        name: 'Gwenn Stacy',
+        birthday: '10 Avril',
+    },
+    {
+        name: 'Thomas Price',
+        birthday: '20 Mai',
+    },
+];
+
+function greetsAllBirthday(date) {
+    for (const index in users) {
+        const user = users[index];
+        const greeting = getUserGreeting(user, date);
+        if (greeting) {
+            return greeting;
+        }
+    }
+    return botResponse('Joyeux anniversaire Michel Anniversaire');
+}
+function getUserGreeting(user, date) {
+    if (date.startsWith(user.birthday)) {
+        return botResponse(`Joyeux anniversaire ${user.name}`);
+    }
+}
+
+module.exports = { bot, greetsAllBirthday };
