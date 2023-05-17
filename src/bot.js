aa53f58c4c2fccdc1a1f2fcc004adc4a1a8d83a6
Laetitia Caffarelli <113118386+LCaffarelli@users.noreply.github.com>
2023-05-17 11:52:50 +0200

    add hello + name (#10)
    * add hello + name
    
    * add hello with the sender's name

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,7 @@
-function bot() {
+function bot(message) {
+    if (message?.message?.sender?.displayName) {
+        return `Hello ${message.message.sender.displayName}!`;
+    }
     return 'Hello World!';
 }
-
 module.exports = bot;
