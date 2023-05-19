82f393dc6d4853b7042eeeb2808a73db000a45db
Laetitia Caffarelli <113118386+LCaffarelli@users.noreply.github.com>
2023-05-19 11:41:33 +0200

    Feature/bot 003 (#12)
    * add hello + name
    
    * add hello with the sender's name
    
    * add function to wish a happy birthday every day to Michel and to Thomas and Gwenn on their birthday
    
    * add function to greet birthday to several users. Need some refactoring
    
    ---------
    
    Co-authored-by: jvolant <118439323+CodeFather44@users.noreply.github.com>

--- src/birthdayBot.test.js
+++ src/birthdayBot.test.js
@@ -2,17 +2,42 @@ const birthdayBot = require('./birthdayBot');
 
 const expectedMichelBirthdayMessage = 'Joyeux anniversaire Michel Anniversaire';
 const thomasPriceBirthday = 'Joyeux anniversaire Thomas Price';
+
+
 describe('birthdayBot', () => {
+    const users = [
+        {
+            name: 'Gwenn Stacy',
+            birthday: '10 Avril',
+        },
+        {
+            name: 'Thomas Price',
+            birthday: '20 Mai',
+        },
+        {
+            name: 'Peter Parker',
+            birthday: '20 Mai',
+        },
+    ];
     test('Should wish birthday to Michel everyday', () => {
-        expect(birthdayBot.greetsBirthday(' ')).toStrictEqual(expectedMichelBirthdayMessage);
+        expect(birthdayBot.greetsBirthdays(' ')).toStrictEqual(expectedMichelBirthdayMessage);
     });
     test('Should wish happy birthday to Thomas price on the 20th May 2023', () => {
-        expect(birthdayBot.greetsBirthday('20 Mai 2023')).toStrictEqual(thomasPriceBirthday);
+        expect(birthdayBot.greetsUserBirthday(users[1], '20 Mai 2023')).toStrictEqual(thomasPriceBirthday);
     });
-    test('Should wish happy birthday to Thomas price on the 10th April 2023', () => {
-        expect(birthdayBot.greetsBirthday('10 Avril 2023')).toStrictEqual('Joyeux anniversaire Gwenn Stacy');
+    test('Should wish happy birthday to Gwenn Stacy on the 10th April 2023', () => {
+        expect(birthdayBot.greetsUserBirthday(users[0], '10 Avril 2023')).toStrictEqual(
+            'Joyeux anniversaire Gwenn Stacy',
+        );
     });
     test('Should wish happy birthday to Thomas price every 20th May', () => {
-        expect(birthdayBot.greetsBirthday('20 Mai 2060')).toStrictEqual(thomasPriceBirthday);
+        expect(birthdayBot.greetsUserBirthday(users[1], '20 Mai 2060')).toStrictEqual(thomasPriceBirthday);
+    });
+    test('Should return happy birthday to users with the same birthday', () => {
+        expect(birthdayBot.getBirthdaysToGreet('20 Mai')).toMatchObject([
+            'Joyeux anniversaire Thomas Price',
+            'Joyeux anniversaire Peter Parker',
+        ]);
+
     });
 });
