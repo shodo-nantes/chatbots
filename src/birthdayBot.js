9a22604e0870c507f528cfce26901567a5ed94e5
clamc068 <laetitia.caffarelli@gmail.com>
2023-05-22 10:26:18 +0200

    Revert "add json file with users and import in birthdayBot.js. Code refactored.Tests ok"
    This reverts commit 4c063c092a96dde6f9f0a27963e93410bd49982e.

--- src/birthdayBot.js
+++ src/birthdayBot.js
@@ -1,6 +1,18 @@
-const jsonData = require('./users.json');
+const users = [
+    {
+        name: 'Gwenn Stacy',
+        birthday: '10 Avril',
+    },
+    {
+        name: 'Thomas Price',
+        birthday: '20 Mai',
+    },
+    {
+        name: 'Peter Parker',
+        birthday: '20 Mai',
+    },
+];
 
-//Souhaite l'anniversaire des employés.
 function greetsBirthdays(date) {
     //Si la fonction retourne une valeur alors on la retourne
     if (getBirthdaysToGreet(date)) {
@@ -10,21 +22,40 @@ function greetsBirthdays(date) {
     return 'Joyeux anniversaire Michel Anniversaire';
 }
 
-//Cherche s'il y a des anniversaires à souhaiter.
 function getBirthdaysToGreet(date) {
-    //utilisation de la programmation fonctionnelle, on definit ce que l'on souhaite recuperer, plus rapide à utiliser que des boucles
-    //dans filter on fait une fonction qui nous permet de recuperer les valeurs de tous les utilisateurs dont la date d'anniversaire commence par celle en parametre.
-    //dans map on fait une fonction qui nous permet de renvoyer une valeur qui souhaite l'anniversaire des valeurs trouvées precedement avec le nom de la personne associée.
-    const greeting = jsonData.users
-        .filter((user) => date.startsWith(user.birthday))
-        .map((user) => `Joyeux anniversaire ${user.name}`);
+    // on instancie la variable greeting en dehors de la boucle
+    const greeting = [];
+
+    //On fait une boucle pour lire nos utilisateurs
+    for (const index in users) {
+        // On récupère leur index et on l'assigne à un user
+        const user = users[index];
+
+        //On verifie que la fonction renvoie une valeur.
+        if (greetsUserBirthday(user, date)) {
+            //Si oui, on l'ajoute au tableau greeting.
+            greeting.push(greetsUserBirthday(user, date));
+
+            //La methode suivante est équivalente :
+            //const birthday = greetsUserBirthday(user, date);
+            //greeting.push(birthday);
+        }
+    }
 
     //On verifie qu'il y ait bien des gens à qui souhaiter leur anniversaire et que la taille du tableau n'est pas égale à 0
-    //(sinon erreur dans les test car renvoi un tableau vide [])
+    //(sinon erreur dans les test car renvoi un tableau vide []) A refactoriser
     if (greeting && greeting.length > 0) {
         //Si ok on renvoi greeting
         return greeting;
     }
 }
 
-module.exports = { greetsBirthdays, getBirthdaysToGreet };
+//Permet de trouver si un user dont la date d'anniversaire correspond au debut de la date du jour (cad dire sans prendre en compte les années)
+//Et  retourne Joyeux anniversaire avec le nom de la personne.
+function greetsUserBirthday(user, date) {
+    if (date.startsWith(user.birthday)) {
+        return `Joyeux anniversaire ${user.name}`;
+    }
+}
+
+module.exports = { greetsBirthdays, greetsUserBirthday, getBirthdaysToGreet };
