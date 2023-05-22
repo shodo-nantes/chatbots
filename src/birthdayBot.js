3fadcb655f2410790034d930609612ecebbda42b
clamc068 <laetitia.caffarelli@gmail.com>
2023-05-22 10:26:30 +0200

    Revert "add function to greet birthday to several users. Need some refactoring"
    This reverts commit 541c87a639722da88c4033ca21839f6a5f2e9b5b.

--- src/birthdayBot.js
+++ src/birthdayBot.js
@@ -8,54 +8,24 @@ const users = [
         birthday: '20 Mai',
     },
     {
-        name: 'Peter Parker',
+        name: 'Spider Man',
         birthday: '20 Mai',
     },
 ];
-
-function greetsBirthdays(date) {
-    //Si la fonction retourne une valeur alors on la retourne
-    if (getBirthdaysToGreet(date)) {
-        return getBirthdaysToGreet(date);
-    }
-    //Sinon on renvoie Joyeux anniversaire Michel Anniversaire
-    return 'Joyeux anniversaire Michel Anniversaire';
-}
-
-function getBirthdaysToGreet(date) {
-    // on instancie la variable greeting en dehors de la boucle
-    const greeting = [];
-
-    //On fait une boucle pour lire nos utilisateurs
+function greetsBirthday(date) {
     for (const index in users) {
-        // On récupère leur index et on l'assigne à un user
         const user = users[index];
-
-        //On verifie que la fonction renvoie une valeur.
-        if (greetsUserBirthday(user, date)) {
-            //Si oui, on l'ajoute au tableau greeting.
-            greeting.push(greetsUserBirthday(user, date));
-
-            //La methode suivante est équivalente :
-            //const birthday = greetsUserBirthday(user, date);
-            //greeting.push(birthday);
+        const greeting = greetsUserBirthday(user, date);
+        if (greeting) {
+            return greeting;
         }
     }
-
-    //On verifie qu'il y ait bien des gens à qui souhaiter leur anniversaire et que la taille du tableau n'est pas égale à 0
-    //(sinon erreur dans les test car renvoi un tableau vide []) A refactoriser
-    if (greeting && greeting.length > 0) {
-        //Si ok on renvoi greeting
-        return greeting;
-    }
+    return 'Joyeux anniversaire Michel Anniversaire';
 }
-
-//Permet de trouver si un user dont la date d'anniversaire correspond au debut de la date du jour (cad dire sans prendre en compte les années)
-//Et  retourne Joyeux anniversaire avec le nom de la personne.
 function greetsUserBirthday(user, date) {
     if (date.startsWith(user.birthday)) {
         return `Joyeux anniversaire ${user.name}`;
     }
 }
 
-module.exports = { greetsBirthdays, greetsUserBirthday, getBirthdaysToGreet };
+module.exports = { greetsBirthday };
