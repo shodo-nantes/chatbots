5726c2ab1b6b2939f8f4c83c3f739f2f792a6ee3
clamc068 <laetitia.caffarelli@gmail.com>
2023-05-22 16:02:03 +0200

    change to compare birthday date to today date + add luxon library to format date + refactoring + tests modified

--- src/birthdayBot.js
+++ src/birthdayBot.js
@@ -1,30 +1,34 @@
+//Imports pour le fichier json ou sont les user ET DateTime pour la lib afin de gerer les dates
 const jsonData = require('./users.json');
+const { DateTime } = require('luxon');
 
 //Souhaite l'anniversaire des employés.
-function greetsBirthdays(date) {
-    //Si la fonction retourne une valeur alors on la retourne
-    if (getBirthdaysToGreet(date)) {
-        return getBirthdaysToGreet(date);
-    }
-    //Sinon on renvoie Joyeux anniversaire Michel Anniversaire
-    return 'Joyeux anniversaire Michel Anniversaire';
-}
-
-//Cherche s'il y a des anniversaires à souhaiter.
-function getBirthdaysToGreet(date) {
+function greetsBirthdays() {
     //utilisation de la programmation fonctionnelle, on definit ce que l'on souhaite recuperer, plus rapide à utiliser que des boucles
-    //dans filter on fait une fonction qui nous permet de recuperer les valeurs de tous les utilisateurs dont la date d'anniversaire commence par celle en parametre.
+    //dans filter on appelle la fonction comparesUserDateToToday
     //dans map on fait une fonction qui nous permet de renvoyer une valeur qui souhaite l'anniversaire des valeurs trouvées precedement avec le nom de la personne associée.
-    const greeting = jsonData.users
-        .filter((user) => date.startsWith(user.birthday))
+
+    const greetings = jsonData.users
+        .filter((user) => getUserIfBirthday(user))
         .map((user) => `Joyeux anniversaire ${user.name}`);
 
-    //On verifie qu'il y ait bien des gens à qui souhaiter leur anniversaire et que la taille du tableau n'est pas égale à 0
-    //(sinon erreur dans les test car renvoi un tableau vide [])
-    if (greeting && greeting.length > 0) {
-        //Si ok on renvoi greeting
-        return greeting;
+    //Comme on doit souhaiter tous les jours l'anniversaire de Michel, on l'ajoute automatiquement au tableau greeting
+    greetings.push('Joyeux anniversaire Michel Anniversaire');
+
+    //On retourne les valeurs dans greetings
+    return greetings;
+}
+
+//on fait une fonction qui nous permet de comparer la date du jour avec la date de naissance d'un user,
+//On retourne le user si y a un match !
+function getUserIfBirthday(user) {
+    //On fait des constantes avec la date du jour et de l'anniversaire pour plus de clarté
+    const today = DateTime.now();
+    const userBirthday = DateTime.fromFormat(user.birthday, 'dd/MM/yyyy');
+
+    if (today.day === userBirthday.day && today.month === userBirthday.month) {
+        return user;
     }
 }
 
-module.exports = { greetsBirthdays, getBirthdaysToGreet };
+module.exports = { greetsBirthdays, getUserIfBirthday };
--- src/birthdayBot.test.js
+++ src/birthdayBot.test.js
@@ -1,14 +1,27 @@
+const { DateTime } = require('luxon');
+const jsonData = require('./users.json');
 const birthdayBot = require('./birthdayBot');
 
 describe('birthdayBot', () => {
     test('Should wish birthday to Michel everyday', () => {
-        expect(birthdayBot.greetsBirthdays(' ')).toStrictEqual('Joyeux anniversaire Michel Anniversaire');
+        expect(birthdayBot.greetsBirthdays()).toContain('Joyeux anniversaire Michel Anniversaire');
     });
 
     test('Should return happy birthday to users with the same birthday', () => {
-        expect(birthdayBot.getBirthdaysToGreet('20 Mai')).toMatchObject([
+        //Permet de modifier la date d'anniversaire de Thomas et Peter à celle du jour
+        //Pour eviter d'avoir à changer la date dans le fichier json pour que le tests fonctionne tous les jours
+        //On appelle la fonction map qui
+        jsonData.users.map((user) => {
+            if (user.name === 'Thomas Price' || user.name === 'Peter Parker') {
+                user.birthday = DateTime.now().toFormat('dd/MM/yyyy');
+            }
+            return user;
+        });
+        //Attend que l'on souhaite l'anniversaire de Thomas, Peter et Michel
+        expect(birthdayBot.greetsBirthdays()).toMatchObject([
             'Joyeux anniversaire Thomas Price',
             'Joyeux anniversaire Peter Parker',
+            'Joyeux anniversaire Michel Anniversaire',
         ]);
     });
 });
