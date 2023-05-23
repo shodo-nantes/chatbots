5507e951d685b9d6c6971ce6990f9a79f5fa1676
Laetitia Caffarelli <113118386+LCaffarelli@users.noreply.github.com>
2023-05-23 09:34:20 +0200

    Feature/bot 003 (#15)
    * add hello + name
    
    * add hello with the sender's name
    
    * add function to wish a happy birthday every day to Michel and to Thomas and Gwenn on their birthday
    
    * add function to greet birthday to several users. Need some refactoring
    
    * add json file with users and import in birthdayBot.js. Code refactored.Tests ok
    
    * Revert "add json file with users and import in birthdayBot.js. Code refactored.Tests ok"
    
    This reverts commit 4c063c092a96dde6f9f0a27963e93410bd49982e.
    
    * Revert "add function to greet birthday to several users. Need some refactoring"
    
    This reverts commit 541c87a639722da88c4033ca21839f6a5f2e9b5b.
    
    * add files after revert
    
    * change to compare birthday date to today date + add luxon library to format date + refactoring + tests modified
    
    * add test to verify we don't wish somebody's birthday when it's not
    
    * modification on comments
    
    * harmonization for linter

--- src/birthdayBot.test.js
+++ src/birthdayBot.test.js
@@ -1,12 +1,39 @@
+const { DateTime } = require('luxon');
+const jsonData = require('./users.json');
 const birthdayBot = require('./birthdayBot');
+
 describe('birthdayBot', () => {
     test('Should wish birthday to Michel everyday', () => {
-        expect(birthdayBot.greetsBirthdays(' ')).toStrictEqual('Joyeux anniversaire Michel Anniversaire');
+        expect(birthdayBot.greetsBirthdays()).toContain('Joyeux anniversaire Michel Anniversaire');
     });
+
     test('Should return happy birthday to users with the same birthday', () => {
-        expect(birthdayBot.getBirthdaysToGreet('20 Mai')).toMatchObject([
+        //Permet de modifier la date d'anniversaire de Thomas et Peter à celle du jour
+        //Pour eviter d'avoir à changer la date dans le fichier json pour que le tests fonctionne tous les jours
+        //On appelle la fonction map qui
+        jsonData.users.map((user) => {
+            if (user.name === 'Thomas Price' || user.name === 'Peter Parker') {
+                user.birthday = DateTime.now().toFormat('dd/MM/yyyy');
+            }
+            return user;
+        });
+        //Attend que l'on souhaite l'anniversaire de Thomas, Peter et Michel
+        expect(birthdayBot.greetsBirthdays()).toContain(
             'Joyeux anniversaire Thomas Price',
             'Joyeux anniversaire Peter Parker',
-        ]);
+            'Joyeux anniversaire Michel Anniversaire',
+        );
+    });
+
+    test("Should not return user if it's not their birthday", () => {
+        jsonData.users.map((user) => {
+            const today = DateTime.now();
+            const todayMinusOneDay = today.minus(86_400_000);
+            if (user.name === 'Gwenn Stacy') {
+                user.birthday = todayMinusOneDay.toFormat('dd/MM/yyyy');
+            }
+            return user;
+        });
+        expect(birthdayBot.greetsBirthdays()).not.toContain('Joyeux anniversaire Gwenn Stacy');
     });
 });
