ce7485248ca65657e20f8fd436fa274913f0fb44
clamc068 <laetitia.caffarelli@gmail.com>
2023-05-23 08:27:16 +0200

    modification on comments

--- src/birthdayBot.js
+++ src/birthdayBot.js
@@ -1,18 +1,18 @@
-//Imports pour le fichier json ou sont les user ET DateTime pour la lib afin de gerer les dates
+//Imports pour le fichier json ou sont les users ET DateTime pour la lib afin de gerer les dates
 const jsonData = require('./users.json');
 const { DateTime } = require('luxon');
 
 //Souhaite l'anniversaire des employés.
 function greetsBirthdays() {
     //utilisation de la programmation fonctionnelle, on definit ce que l'on souhaite recuperer, plus rapide à utiliser que des boucles
-    //dans filter on appelle la fonction comparesUserDateToToday
-    //dans map on fait une fonction qui nous permet de renvoyer une valeur qui souhaite l'anniversaire des valeurs trouvées precedement avec le nom de la personne associée.
+    //dans filter on appelle la fonction getUserIfBirthday qui va parcourir users et renvoyer une ou des valeurs s'il y a occurence
+    //dans map on fait une fonction qui nous permet de réaliser un traitement sur les valeurs trouvées en renvoyant 'Joyeux anniversaire'avec le nom de la personne associée.
 
     const greetings = jsonData.users
         .filter((user) => getUserIfBirthday(user))
         .map((user) => `Joyeux anniversaire ${user.name}`);
 
-    //Comme on doit souhaiter tous les jours l'anniversaire de Michel, on l'ajoute automatiquement au tableau greeting
+    //Comme on doit souhaiter tous les jours l'anniversaire de Michel, on l'ajoute automatiquement au tableau greetings
     greetings.push('Joyeux anniversaire Michel Anniversaire');
 
     //On retourne les valeurs dans greetings
