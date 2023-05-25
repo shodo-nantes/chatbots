b18cb2445476773b82771da53993ee030231ce0e
clamc068 <laetitia.caffarelli@gmail.com>
2023-05-25 14:35:22 +0200

    code cleaned

--- src/birthdayBot.test.js
+++ src/birthdayBot.test.js
@@ -8,16 +8,13 @@ describe('birthdayBot', () => {
     });
 
     test('Should return happy birthday to users with the same birthday', () => {
-        //Permet de modifier la date d'anniversaire de Thomas et Peter à celle du jour
-        //Pour eviter d'avoir à changer la date dans le fichier json pour que le tests fonctionne tous les jours
-        //On appelle la fonction map qui
         jsonData.users.map((user) => {
             if (user.name === 'Thomas Price' || user.name === 'Peter Parker') {
                 user.birthday = DateTime.now().toFormat('dd/MM/yyyy');
             }
             return user;
         });
-        //Attend que l'on souhaite l'anniversaire de Thomas, Peter et Michel
+
         expect(birthdayBot.greetsBirthdays()).toContain(
             'Joyeux anniversaire Thomas Price',
             'Joyeux anniversaire Peter Parker',
