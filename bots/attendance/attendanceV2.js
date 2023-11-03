bb706a21163c830076163c405f266cff4f12e54e
Mara Martini <mma_@gmail.com>
2023-11-03 11:32:14 +0100

    Require hello_world effacé

--- bots/attendance/attendanceV2.js
+++ bots/attendance/attendanceV2.js
@@ -1,8 +1,6 @@
 const { App } = require('@slack/bolt');
 require('dotenv').config();
 
-const handleAppHomeOpened = require('./helloWorld');
-
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     signingSecret: process.env.SLACK_SIGNING_SECRET,
